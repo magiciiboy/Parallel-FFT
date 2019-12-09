a28c09c8c097d8350f69d6423d92c3f1aac75901
Tung Huynh <tung.huynh@seedcom.vn>
2019-12-09 17:46:13 -0600

    Minor update

--- sequential/fft_serial.c
+++ sequential/fft_serial.c
@@ -150,7 +150,7 @@ int main ( )
 
         mflops = flops / 1.0E+06 / ctime;
 
-        printf ( "  %20.0f  %12e  %12e  %12f\n", flops, ctime, ctime / ( double ) ( 2 * nits ), mflops );
+        printf ( "  %20.0f  %12.5f  %12e  %12f\n", flops, ctime, ctime / ( double ) ( 2 * nits ), mflops );
       }
     }
     if ( ( ln2 % 4 ) == 0 ) 
